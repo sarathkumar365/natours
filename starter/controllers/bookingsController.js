7f06d11917b0b440fe303b98dc396776c93f8dc0
sarathkumar365 <sarathkumark.s365@gmail.com>
2021-07-09 11:22:28 +0530

    stripe imp 6

--- starter/controllers/bookingsController.js
+++ starter/controllers/bookingsController.js
@@ -55,6 +55,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
+  console.log(session);
+
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.display_items[0].amount / 100;
