9fb0e1e1dbc6d1f200b6feedba2ee9758e2ab9e4
sarathkumar365 <sarathkumark.s365@gmail.com>
2021-07-10 18:39:07 +0530

    HEROKU ERROR 14

--- starter/controllers/bookingsController.js
+++ starter/controllers/bookingsController.js
@@ -64,7 +64,7 @@ const createBookingCheckout = async (session) => {
     session.object.amount_total
   );
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
+  const user = (await User.findOne({ email: session.customer_email }))._id;
   const price = session.object.amount_total / 100;
   console.log(`details ${(tour, user, price)}`);
   await Bookings.create({ tour, user, price });
